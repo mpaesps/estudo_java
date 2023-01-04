b3b92c77a94ff17ddd17b0199d6b8ff04d6cc10c
Mayara Paes <mpaesps133@gmail.com>
2023-01-03 22:06:55 -0300

    primeira atividade de arrays

---
+++ src/primeiro_array_atividade2.java
@@ -0,0 +1,34 @@
+public class primeiro_array_atividade2 {
+    public static void main (String[] args){
+
+        //declarando um array de inteiros
+        int umArray[];
+
+        //alocando memoria para 10 inteiros
+        umArray = new int[10];
+
+        //inicializando os elementos do array
+        umArray[0] =  100;
+        umArray[1] =  200;
+        umArray[2] =  300;
+        umArray[3] =  400;
+        umArray[4] =  500;
+        umArray[5] =  600;
+        umArray[6] =  700;
+        umArray[7] =  800;
+        umArray[8] =  900;
+        umArray[9] =  1000;
+
+        System.out.println("elemento no indice 0: " + umArray[0]);
+        System.out.println("elemento no indice 1: " + umArray[1]);
+        System.out.println("elemento no indice 2: " + umArray[2]);
+        System.out.println("elemento no indice 3: " + umArray[3]);
+        System.out.println("elemento no indice 4: " + umArray[4]);
+        System.out.println("elemento no indice 5: " + umArray[5]);
+        System.out.println("elemento no indice 6: " + umArray[6]);
+        System.out.println("elemento no indice 7: " + umArray[7]);
+        System.out.println("elemento no indice 8: " + umArray[8]);
+        System.out.println("elemento no indice 9: " + umArray[9]);
+
+    }
+}
