e7293f3af6ed2d84d0af7da73e05240b6f761a0b
Mayara Paes <mpaesps133@gmail.com>
2023-01-03 22:14:32 -0300

    segunda atividade de arrays, shortcut syntax

---
+++ src/segundo_array_atividade3.java
@@ -0,0 +1,11 @@
+public class segundo_array_atividade3 {
+    public static void main(String[] args){
+        int umArray[] = {100,200,300,400,500};
+
+        System.out.println("Elemento 1 no indice 0: " + umArray[0]);
+        System.out.println("Elemento 2 no indice 1: " + umArray[1]);
+        System.out.println("Elemento 3 no indice 2: " + umArray[2]);
+        System.out.println("Elemento 4 no indice 3: " + umArray[3]);
+        System.out.println("Elemento 5 no indice 4: " + umArray[4]);
+    }
+}
