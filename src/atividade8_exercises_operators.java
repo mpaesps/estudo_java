4c3e3ac00f79050fd326ef4ff86367edbc127c98
Mayara Paes <mpaesps133@gmail.com>
2023-01-05 21:18:26 -0300

    atividade 8 - atividade de operadores da documentacao - correcao

--- src/atividade8_exercises_operators.java
+++ src/atividade8_exercises_operators.java
@@ -21,19 +21,19 @@ public class atividade8_exercises_operators {
         int result = 1 + 2; // result is now 3
         System.out.println(result);
 
-        result = --result; // result is now 2
+        result -= 1 ; // result is now 2
         System.out.println(result);
 
-        result = result * 2; // result is now 4
+        result *= 2 ; // result is now 4
         System.out.println(result);
 
-        result = result / 2; // result is now 2
+        result /=  2; // result is now 2
         System.out.println(result);
 
-        result = result + 8; // result is now 10
+        result += 8; // result is now 10
         System.out.println(result);
 
-        result = result % 7; // result is now 3
+        result &= 7; // result is now 3
         System.out.println(result);
 
 
