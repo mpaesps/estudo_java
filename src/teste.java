336fe6cd680dd731af48f8842cf5e942757ff2db
Mayara Paes <mpaesps133@gmail.com>
2023-01-07 20:48:08 -0300

    atividade 9 - atividade if else

---
+++ src/teste.java
@@ -0,0 +1,8 @@
+public class teste {
+    public static void main(String[] args){
+        int i = 10;
+        int n = i++%5;
+        System.out.println(i);
+        System.out.println(n);
+    }
+}
