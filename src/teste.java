e5714772f41913611fb952e38ead379a50f23e42
Mayara Paes <mpaesps133@gmail.com>
2023-01-07 20:57:48 -0300

    Revert "atividade 9 - atividade if else"
    This reverts commit 336fe6cd680dd731af48f8842cf5e942757ff2db.

--- src/teste.java
+++
@@ -1,8 +0,0 @@
-public class teste {
-    public static void main(String[] args){
-        int i = 10;
-        int n = i++%5;
-        System.out.println(i);
-        System.out.println(n);
-    }
-}
