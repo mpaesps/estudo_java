61b7aeb67e11de11f79f84b9951c061d13b6a705
Mayara Paes <mpaesps133@gmail.com>
2023-01-18 00:34:01 -0300

    adicionando um metodo á classe Triangulo

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -24,10 +24,9 @@ public class Programa  {
         y.b = sc.nextDouble();
         y.c = sc.nextDouble();
 
-        double p = (x.a + x.b + x.c) / 2.0;
-        double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
-        p = (y.a + y.b + y.c) / 2.0;
-        double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+        double areaX = x.area();
+        double areaY = y.area();
+
         System.out.printf("triangulo X area: %.4f%n", areaX);
         System.out.printf("triangulo Y area: %.4f%n", areaY);
         if (areaX > areaY) {
